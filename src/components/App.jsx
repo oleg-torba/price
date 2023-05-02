f54a3c3e76635da7fdf37188f6b80efc6d0abd4c
oleg-torba <100319055+oleg-torba@users.noreply.github.com>
2023-05-02 22:38:57 +0300

    add title

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,263 +1,65 @@
-import  { useState} from 'react';
-
+import { useState } from 'react';
+import { ItemList } from './ContactList/contactList';
 import { Section } from './Section/section';
 
-import Data from "./apiData.json"
+import Data from './apiData.json';
 
-
-export function App () {
-
-  const [inputValue, setInputValue] = useState('')
-  const [gsm, setGsm] = useState([])
-  gsm.sort((a,b) => a.name > b.name ? 1 : -1)
-  const data = Data.shop.items.item
+export function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [gsm, setGsm] = useState([]);
+  gsm.sort((a, b) => (a.name > b.name ? 1 : -1));
+  const data = Data.shop.items.item;
 
   const handleChange = e => {
     const inputValue = e.target.value;
     setInputValue(inputValue);
-    console.log(inputValue)
-  
+    console.log(inputValue);
   };
 
   const handleSubmit = e => {
     e.preventDefault();
-if(inputValue === ''){
-  alert('Заповність поле пошуку')
-  return
-}
+    if (inputValue === '') {
+      alert('Заповність поле пошуку');
+      return;
+    }
 
-   setGsm(data.filter(contact =>
-      contact.name.toLowerCase().includes(inputValue.toLowerCase())
-    ))
-    
-    
-
+    setGsm(
+      data.filter(contact =>
+        contact.name.toLowerCase().includes(inputValue.toLowerCase())
+      )
+    );
   };
 
-
-
-    return (
-
-    
-        <>
-        <Section/>
+  return (
+    <>
+      <Section title= <p className='title'>
+            The application was created as a test task for finding prices for repairs. Thanks to <a href="https://gsm-forsage.com.ua">GSM-FROSAGE</a> and <a href="https://tplus.market">TEXNO+</a> ​​for the provided API. 
+            Junior front end developer: Oleg Torba. 
+          </p>>
         <form className="form" onSubmit={handleSubmit}>
-
-   
-
           <input
             className="formInput"
-            placeholder='Введіть модель для пошуку'
+            placeholder="Введіть модель для пошуку"
             name="data"
             value={inputValue}
+            onChange={handleChange}
+          />
 
-            onChange={handleChange} />
-       
-        <button className="formBtn" type="submit">
-         Search
-        </button>
-      </form>
-     {gsm.length>0 &&  <Section title={gsm.length > 0 && <p>За запитом "{inputValue}" ми знайшли {gsm.length} позицій</p>}/>}
-      <div className="contactBlock">
-        
-          <ul className="contactsList">
-            {gsm.map((item) => {
+          <button className="formBtn" type="submit">
+            Search
+          </button>
+        </form>
+      </Section>
 
-
-
-
-              let priceLCD = Math.ceil(((item.price + 20) * 38.5) / 100) * 100;
-              const changeGlass = priceLCD/2
-
-              let priceCharge = Math.ceil(((item.price + 10) * 38.5) / 100) * 100;
-              if (item.price >= 25) {
-                priceLCD = Math.ceil(((item.price + 25) * 38.5) / 100) * 100;
-              }
-              if (item.price >= 60) {
-                priceLCD = Math.ceil(((item.price + 30) * 38.5) / 100) * 100;
-
-              }
-            
-
-              if (item.name.includes("Дисплей") || item.name.includes('Complete')) {
-                return (
-
-                  <>
-                    <li className="contactsItem" key={item.name}>
-                      <div className="contact">
-                        <span>
-                          <img src={item.image} alt="" width="100" height="100" />
-                        </span>
-                        <span>
-                          <span className="contactValue">Заміна дисплейного модулю:</span> {item.name}
-                        </span>
-                        <span>
-                          <span className="contactValue">Ціна:</span>{priceLCD} грн
-                        </span>
-                        <span>
-                          <span className="contactValue">Заміна скла дисплею:</span>{changeGlass} грн
-                        </span>
-                        <span>
-                          <span className="contactValue">Наявність:</span>{item.quantity_in_stock} шт
-                        </span>
-
-                      </div>
-                    </li>
-                  </>
-                );
-              }
-              if (item.name.includes('Роз\'єм заряд') || item.name.includes('Charge')) {
-                return (
-                  <>
-                    <li className="contactsItem" key={item.name}>
-                      <div className="contact">
-                      <span>
-                          <img src={item.image} alt="" width="100" height="100" />
-                        </span>
-                        <span>
-                          <span className="contactValue">Заміна роз'єму живлення:</span> {item.name}
-                        </span>
-                        <span>
-                          <span className="contactValue">Ціна:</span>
-                          {priceCharge} грн
-                        </span>
-                        <span>
-                          <span className="contactValue">Наявність:</span>{item.quantity_in_stock} шт
-                        </span>
-
-                      </div>
-                    </li>
-                  </>
-                );
-              }
-              if (item.name.includes("Задня")) {
-                return (
-                  <>
-                    <li className="contactsItem" key={item.name}>
-                      <div className="contact">
-                      <span>
-                          <img src={item.image} alt="" width="100" height="100" />
-                        </span>
-                        <span>
-                          <span className="contactValue">Заміна задньої кришки:</span> {item.name}
-                        </span>
-                        <span>
-                          <span className="contactValue">Ціна:</span>{priceCharge} грн
-                        </span>
-                        <span>
-                          <span className="contactValue">Наявність:</span>{item.quantity_in_stock} шт
-                        </span>
-
-                      </div>
-                    </li>
-                  </>
-                );
-              }
-              if (item.name.includes("Камер" || "камер")) {
-                return (
-                  <>
-                    <li className="contactsItem" key={item.name}>
-                      <div className="contact">
-                      <span>
-                          <img src={item.image} alt="" width="100" height="100" />
-                        </span>
-                        <span>
-                          <span className="contactValue">Заміна камери (основна, фронтальна):</span> {item.name}
-                        </span>
-                        <span>
-                          <span className="contactValue">Ціна:</span>
-                          {priceCharge} грн
-                        </span>
-                        <span>
-                          <span className="contactValue">Наявність:</span>{item.quantity_in_stock} шт
-                        </span>
-
-                      </div>
-                    </li>
-                  </>
-                );
-              }
-              if (item.name.includes("Акумулятор")) {
-                return (
-                  <>
-                    <li className="contactsItem" key={item.name}>
-                      <div className="contact">
-                      <span>
-                          <img src={item.image} alt="" width="100" height="100" />
-                        </span>
-                        <span>
-                          <span className="contactValue">Заміна акумулятора:</span> {item.name}
-                        </span>
-                        <span>
-                          <span className="contactValue">Ціна:</span>
-                          {priceCharge} грн
-                        </span>
-                        <span>
-                          <span className="contactValue">Наявність:</span>{item.quantity_in_stock} шт
-                        </span>
-
-                      </div>
-                    </li>
-                  </>
-                );
-              }
-              if (item.name.includes("Бузер")) {
-                return (
-                  <>
-                    <li className="contactsItem" key={item.name}>
-                      <div className="contact">
-                      <span>
-                          <img src={item.image} alt="" width="100" height="100" />
-                        </span>
-                        <span>
-                          <span className="contactValue">Заміна бузера:</span> {item.name}
-                        </span>
-                        <span>
-                          <span className="contactValue">Ціна:</span>
-                          {priceCharge} грн
-                        </span>
-                        <span>
-                          <span className="contactValue">Наявність:</span>{item.quantity_in_stock} шт
-                        </span>
-
-                      </div>
-                    </li>
-                  </>
-                );
-              }
-              if (item.name.includes("Динамік")) {
-                return (
-                  <>
-                    <li className="contactsItem" key={item.name}>
-                      <div className="contact">
-                      <span>
-                          <img src={item.image} alt="" width="100" height="100" />
-                        </span>
-                        <span>
-                          <span className="contactValue">Заміна спікера:</span> {item.name}
-                        </span>
-                        <span>
-                          <span className="contactValue">Ціна:</span>
-                          {priceCharge} грн
-                        </span>
-                        <span>
-                          <span className="contactValue">Наявність:</span>{item.quantity_in_stock} шт
-                        </span>
-
-                      </div>
-                    </li>
-                  </>
-                );
-
-              }else{
-                return null
-              }
-            }
-            )}
-          </ul>
-        </div></>
-    
-      
-    );
-  }
-
+      {gsm.length > 0 && (
+        <Section
+          title=<p>
+            За запитом "{inputValue}" ми знайшли {gsm.length} позицій
+          </p>
+        >
+          <ItemList gsm={gsm} />
+        </Section>
+      )}
+    </>
+  );
+}
--- src/components/ContactList/contactList.jsx
+++ src/components/ContactList/contactList.jsx
@@ -1,6 +1,211 @@
 // /* eslint-disable array-callback-return */
-// import React from 'react';
+import React from 'react';
 // import PropTypes from 'prop-types';
+
+
+
+export function ItemList ({gsm}) {
+    return (
+        <div className="contactBlock">
+        
+          <ul className="contactsList">
+            {gsm.map((item) => {
+
+
+
+
+              let priceLCD = Math.ceil(((item.price + 20) * 38.5) / 100) * 100;
+              const changeGlass = priceLCD/2
+
+              let priceCharge = Math.ceil(((item.price + 10) * 38.5) / 100) * 100;
+              if (item.price >= 25) {
+                priceLCD = Math.ceil(((item.price + 25) * 38.5) / 100) * 100;
+              }
+              if (item.price >= 60) {
+                priceLCD = Math.ceil(((item.price + 30) * 38.5) / 100) * 100;
+
+              }
+            
+
+              if (item.name.includes("Дисплей") || item.name.includes('Complete')) {
+                return (
+
+                  <>
+                    <li className="contactsItem" key={item.name}>
+                      <div className="contact">
+                        <span>
+                          <img src={item.image} alt="" width="100" height="100" />
+                        </span>
+                        <span>
+                          <span className="contactValue">Заміна дисплейного модулю:</span> {item.name}
+                        </span>
+                        <span>
+                          <span className="contactValue">Ціна:</span>{priceLCD} грн
+                        </span>
+                        <span>
+                          <span className="contactValue">Заміна скла дисплею:</span>{changeGlass} грн
+                        </span>
+                        <span>
+                          <span className="contactValue">Наявність:</span>{item.quantity_in_stock} шт
+                        </span>
+
+                      </div>
+                    </li>
+                  </>
+                );
+              }
+              if (item.name.includes('Роз\'єм заряд') || item.name.includes('Charge')) {
+                return (
+                  <>
+                    <li className="contactsItem" key={item.name}>
+                      <div className="contact">
+                      <span>
+                          <img src={item.image} alt="" width="100" height="100" />
+                        </span>
+                        <span>
+                          <span className="contactValue">Заміна роз'єму живлення:</span> {item.name}
+                        </span>
+                        <span>
+                          <span className="contactValue">Ціна:</span>
+                          {priceCharge} грн
+                        </span>
+                        <span>
+                          <span className="contactValue">Наявність:</span>{item.quantity_in_stock} шт
+                        </span>
+
+                      </div>
+                    </li>
+                  </>
+                );
+              }
+              if (item.name.includes("Задня")) {
+                return (
+                  <>
+                    <li className="contactsItem" key={item.name}>
+                      <div className="contact">
+                      <span>
+                          <img src={item.image} alt="" width="100" height="100" />
+                        </span>
+                        <span>
+                          <span className="contactValue">Заміна задньої кришки:</span> {item.name}
+                        </span>
+                        <span>
+                          <span className="contactValue">Ціна:</span>{priceCharge} грн
+                        </span>
+                        <span>
+                          <span className="contactValue">Наявність:</span>{item.quantity_in_stock} шт
+                        </span>
+
+                      </div>
+                    </li>
+                  </>
+                );
+              }
+              if (item.name.includes("Камер" || "камер")) {
+                return (
+                  <>
+                    <li className="contactsItem" key={item.name}>
+                      <div className="contact">
+                      <span>
+                          <img src={item.image} alt="" width="100" height="100" />
+                        </span>
+                        <span>
+                          <span className="contactValue">Заміна камери (основна, фронтальна):</span> {item.name}
+                        </span>
+                        <span>
+                          <span className="contactValue">Ціна:</span>
+                          {priceCharge} грн
+                        </span>
+                        <span>
+                          <span className="contactValue">Наявність:</span>{item.quantity_in_stock} шт
+                        </span>
+
+                      </div>
+                    </li>
+                  </>
+                );
+              }
+              if (item.name.includes("Акумулятор")) {
+                return (
+                  <>
+                    <li className="contactsItem" key={item.name}>
+                      <div className="contact">
+                      <span>
+                          <img src={item.image} alt="" width="100" height="100" />
+                        </span>
+                        <span>
+                          <span className="contactValue">Заміна акумулятора:</span> {item.name}
+                        </span>
+                        <span>
+                          <span className="contactValue">Ціна:</span>
+                          {priceCharge} грн
+                        </span>
+                        <span>
+                          <span className="contactValue">Наявність:</span>{item.quantity_in_stock} шт
+                        </span>
+
+                      </div>
+                    </li>
+                  </>
+                );
+              }
+              if (item.name.includes("Бузер")) {
+                return (
+                  <>
+                    <li className="contactsItem" key={item.name}>
+                      <div className="contact">
+                      <span>
+                          <img src={item.image} alt="" width="100" height="100" />
+                        </span>
+                        <span>
+                          <span className="contactValue">Заміна бузера:</span> {item.name}
+                        </span>
+                        <span>
+                          <span className="contactValue">Ціна:</span>
+                          {priceCharge} грн
+                        </span>
+                        <span>
+                          <span className="contactValue">Наявність:</span>{item.quantity_in_stock} шт
+                        </span>
+
+                      </div>
+                    </li>
+                  </>
+                );
+              }
+              if (item.name.includes("Динамік")) {
+                return (
+                  <>
+                    <li className="contactsItem" key={item.name}>
+                      <div className="contact">
+                      <span>
+                          <img src={item.image} alt="" width="100" height="100" />
+                        </span>
+                        <span>
+                          <span className="contactValue">Заміна спікера:</span> {item.name}
+                        </span>
+                        <span>
+                          <span className="contactValue">Ціна:</span>
+                          {priceCharge} грн
+                        </span>
+                        <span>
+                          <span className="contactValue">Наявність:</span>{item.quantity_in_stock} шт
+                        </span>
+
+                      </div>
+                    </li>
+                  </>
+                );
+
+              }else{
+                return null
+              }
+            }
+            )}
+          </ul>
+        </div>
+    )
+}
 
 // export function Contact({ gsm }) {
 
