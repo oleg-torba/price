aa5e96a09b1c04eb7bcda48421e69d0b395f3dfa
oleg-torba <100319055+oleg-torba@users.noreply.github.com>
2023-05-05 23:05:11 +0300

    css

--- src/components/Servicelist/ServiceList.jsx
+++ src/components/Servicelist/ServiceList.jsx
@@ -6,58 +6,58 @@ export function ServiceList (){
         <ul className="serviceItem">
             <li className="serviceList">
            
-            <p className="serviceTown">Львів, вул. Кульпарківська, 59</p>
+            <p className="serviceTown">Львів, Кульпарківська, 59</p>
                 <p>098-188-28-58{' '}<a classname="serviceLink" href="https://t.me/+TuRc4zxZnPZxh3vI"><FaTelegram RiTelegramLine/></a></p>
            
                
                  </li>
                  <li className="serviceList">
                 
-                <p className="serviceTown">Львів, вул. Городоцька, 177</p>
-                <p>067-691-78-27{' '} <a  href="https://t.me/+TuRc4zxZnPZxh3vl" classname="serviceLink"><FaTelegram RiTelegramLine/></a></p>
+                <p className="serviceTown">Львів, Городоцька, 177</p>
+                <p>067-691-78-27{' '} <a  href="https://t.me/+TuRc4zxZnPZxh3vI" classname="serviceLink"><FaTelegram RiTelegramLine/></a></p>
                
                  </li>
               
                  <li className="serviceList">
                 
                  <p className="serviceTown">Бібрка, Ринкова, 2а</p>
-                <p>067-862-99-19{' '} <a  href="https://t.me/+TuRc4zxZnPZxh3vl" classname="serviceLink"><FaTelegram RiTelegramLine/></a></p>
+                <p>067-862-99-19{' '} <a  href="https://t.me/+TuRc4zxZnPZxh3vI" classname="serviceLink"><FaTelegram RiTelegramLine/></a></p>
              
                  </li>
                  <li className="serviceList">
                 
-                 <p className="serviceTown">Мостиська, площа Ринок, 17/б</p>
-                <p>098-188-28-49{' '} <a  href="https://t.me/+TuRc4zxZnPZxh3vl" classname="serviceLink"><FaTelegram RiTelegramLine/></a></p>
+                 <p className="serviceTown">Мостиська, пл.Ринок, 17/б</p>
+                <p>098-188-28-49{' '} <a  href="https://t.me/+TuRc4zxZnPZxh3vI" classname="serviceLink"><FaTelegram RiTelegramLine/></a></p>
                
                  </li>
                  <li className="serviceList">
              
                  <p className="serviceTown">Новояворівськ, Бандери, 18</p>
-                <p>097-365-28-42{' '} <a  href="https://t.me/+TuRc4zxZnPZxh3vl" classname="serviceLink"><FaTelegram RiTelegramLine/></a></p>
+                <p>097-365-28-42{' '} <a  href="https://t.me/+TuRc4zxZnPZxh3vI" classname="serviceLink"><FaTelegram RiTelegramLine/></a></p>
 
                  </li>
                  <li className="serviceList">
                
                  <p className="serviceTown">Недригайлів, Незалежності, 8</p>
-                <p>098-904-49-44{' '} <a  href="https://t.me/+TuRc4zxZnPZxh3vl" classname="serviceLink"><FaTelegram RiTelegramLine/></a></p>
+                <p>098-904-49-44{' '} <a  href="https://t.me/+TuRc4zxZnPZxh3vI" classname="serviceLink"><FaTelegram RiTelegramLine/></a></p>
         
                  </li>
                  <li className="serviceList">
                
                  <p className="serviceTown">Рогатин, Шевченка, 3а</p>
-                <p>068-588-29-28{' '} <a  href="https://t.me/+TuRc4zxZnPZxh3vl" classname="serviceLink"><FaTelegram RiTelegramLine/></a></p>
+                <p>068-588-29-28{' '} <a  href="https://t.me/+TuRc4zxZnPZxh3vI" classname="serviceLink"><FaTelegram RiTelegramLine/></a></p>
                
                  </li>
                  <li className="serviceList">
                 
                  <p className="serviceTown">Сарни</p>
-                <p>067-671-49-38{' '} <a  href="https://t.me/+TuRc4zxZnPZxh3vl" classname="serviceLink"><FaTelegram RiTelegramLine/></a></p>
+                <p>067-671-49-38{' '} <a  href="https://t.me/+TuRc4zxZnPZxh3vI" classname="serviceLink"><FaTelegram RiTelegramLine/></a></p>
    
                  </li>
                  <li className="serviceList">
                 
                  <p className="serviceTown">Трускавець, Стебницька, 58</p>
-                <p>067-746-03-90{' '} <a  href="https://t.me/+TuRc4zxZnPZxh3vl" classname="serviceLink"><FaTelegram RiTelegramLine/></a></p>
+                <p>067-746-03-90{' '} <a  href="https://t.me/+TuRc4zxZnPZxh3vI" classname="serviceLink"><FaTelegram RiTelegramLine/></a></p>
                
                  </li>
                  
